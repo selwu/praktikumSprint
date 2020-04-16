95e4ebe5816e14a67384f6c168fd15b519c84bf7
selwu <selwu@mail.ru>
2020-04-16 08:27:40 +0300

    end 5 sprint

--- 12-count-zeros/script.js
+++ 12-count-zeros/script.js
@@ -6,9 +6,25 @@
  *
  */
 
-function countZeros(n) {}
+function countZeros(n) {
+  if (typeof n !== 'number') alert('error');
+  let result = 0;
+
+  const countNumber = function (num) {
+    const str = String(num);
+    for (let s = 0; s < str.length; s += 1) {
+      if (str[s] === '0') result += 1;
+    }
+  };
+
+  for (let i = 1; i <= n; i += 1) {
+    countNumber(i);
+  }
+
+  return result;
+}
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(countZeros(342)); // 2 – два нуля, по одному в числах 10 и 20
+console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
 console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
